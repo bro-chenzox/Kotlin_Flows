09d662ed62815038eed63660fe6cc650f6ea16ab
Bro Chenzox <spalnavy@gmail.com>
2021-02-18 11:47:59 +0300

    Simple flow example

--- app/src/main/java/com/palchak/sergey/kotlinflows/MainActivity.kt
+++ app/src/main/java/com/palchak/sergey/kotlinflows/MainActivity.kt
@@ -2,11 +2,31 @@ package com.palchak.sergey.kotlinflows
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Producer
+        val flow = flow {
+            for (i in 1..10) {
+                emit("Hello World!")
+                delay(1000)
+            }
+        }
+
+        // Consumer
+        GlobalScope.launch {
+            flow.collect {
+                println(it)
+            }
+        }
     }
 
 }
